e5d9ff443a026cc7317d3300966d5adc694ba9ba
Ismail Aksoy <iaksoy@seanapse.io>
2019-03-11 00:06:05 +0300

    todo: resources tests

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
-import os
 import unittest
-import tempfile
 
 from peewee import *
-from datetime import datetime
 
 from app import app
-from models import Todo
+import models
+
+MODELS = [models.Todo]
+test_db = SqliteDatabase(':memory:')
 
 
-class AppTest(unittest.TestCase):
-
+class TestApp(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
@@ -19,3 +18,25 @@ class AppTest(unittest.TestCase):
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertIn(b'My TODOs!', resp.data)
+
+
+class TestTodoModel(unittest.TestCase):
+    def setUp(self):
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        test_db.drop_tables(MODELS)
+        test_db.close()
+
+    def test_todo_table(self):
+        self.assertTrue(models.Todo.table_exists())
+
+    def test_todo_creation(self):
+        models.Todo.create(name="testthiscode")
+        todo = models.Todo.select().where(models.Todo.name == 'testthiscode')
+        self.assertEqual(todo.count(), 1)
+
+    def test_model_initialization(self):
+        models.initialize()
